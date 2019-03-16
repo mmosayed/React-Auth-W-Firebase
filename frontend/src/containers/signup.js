ec5f6b0eb4ce7ae68562bd9398b02449cd9fb840
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-03-16 14:25:42 -0400

    added auth

--- frontend/src/containers/signup.js
+++ frontend/src/containers/signup.js
@@ -3,21 +3,50 @@ import firebase from '../firebase';
 
 export default class Signup extends React.Component {
 
+  state = {
+    email: '',
+    password: '',
+    error: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log('Returns: ', response);
+      })
+      .catch(err => {
+        const { message } = err;
+        this.setState({ error: message });
+      })
+  }
+
   render() {
+    const { email, password, error } = this.state;
+    const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
+
     return (
       <>
+        {displayError}
         <form>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" />
+            <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" placeholder="Password" />
+            <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
           </div>
-          <button type="submit" className="btn btn-primary">Sign Up</button>
+          <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Sign Up</button>
         </form>
       </>
     )
   }
 }
+
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,14 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const config = {};
+const config = {
+  apiKey: "AIzaSyDnX-V1IEQDlgNv1XwraQA9jXi3pE8Yomk",
+  authDomain: "dumb-uploader.firebaseapp.com",
+  databaseURL: "https://dumb-uploader.firebaseio.com",
+  projectId: "dumb-uploader",
+  storageBucket: "dumb-uploader.appspot.com",
+  messagingSenderId: "370924651634"
+};
 
 app.initializeApp(config);
 
